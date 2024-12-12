cc82e7441b1978e8bfb80dcd39ed2835e1e4856f
Ryan C <rjchou@icloud.com>
2024-12-11 19:10:05 -0800

    Basic

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #endif
 #define _USE_MATH_DEFINES
 #include <cstdio>
+#include <thread>
+#include <atomic>
 #include "TextHelp.h"
 
 
@@ -31,14 +33,38 @@ Uint32 mousebitmask;
 bool darkmode = true;
 
 
+
+std::string key = "4613~JW2h7AZXUK6LBkEnKxtPtmrJ22cUeUVGyfYCUhnuNNKat46nVAVN9ufUvaFUxvZA";
+std::string CourseBase = "https://creanlutheran.instructure.com/api/v1/"; // conversations, courses
+
+
+std::atomic_int stage;
+int tcount = 4;
+
+
 float deltime = 0;
 float then = 0;
 float now = 0;
 
 
+struct course{
+    std::string name;
+    int id;
+};
+
+
+struct item{
+    std::string name;
+    int id;
+};
+
+
+std::vector<course> courses;
+std::vector<std::vector<item> > items;
+
+
 std::string command(const char * cmd){
-    std::cout << cmd << std::endl;
-    FILE * pipe = _popen(cmd, "r");
+    FILE * pipe = popen(cmd, "r");
     if (!pipe) return "";
 
     char buffer[128];
@@ -46,8 +72,87 @@ std::string command(const char * cmd){
     while (fgets(buffer, sizeof(buffer), pipe) != nullptr){
         returnv += std::string() + buffer;
     }
-    _pclose(pipe);
+    pclose(pipe);
     return returnv;
+}
+
+
+std::vector<std::string> split(std::string value, std::string key){
+    std::string check;
+    std::string add;
+    std::vector<std::string> returnv;
+    for (int i = 0; i < value.length(); i++){
+        if (key.contains(value[i])){
+            check += value[i];
+        }
+        else{
+            add += check + value[i];
+            check = "";
+        }
+        if (check == key){
+            returnv.push_back(add);
+            check = "";
+            add = "";
+        }
+    }
+    returnv.push_back(add);
+    return returnv;
+}
+
+
+std::string splice(std::string value, int start, int end){
+    std::string returnv;
+    for (int i = start; i < value.length()-end; i++){
+        returnv += value[i];
+    }
+    return returnv;
+}
+
+
+course ToCourse(std::string value){
+    std::vector<std::string> returnv;
+    std::string add;
+    int depth = 0;
+    for (int i = 0; i < value.length(); i++){
+        if (std::string("([{").contains(value[i])) depth++;
+        elif (std::string(")]}").contains(value[i])) depth--;
+        elif (depth == 0 && std::string(",").contains(value[i])){
+            returnv.push_back(add);
+            add = "";
+        }
+        else add += value[i];
+    }
+    return {split(returnv[1], ":")[1], std::stoi(split(returnv[0], ":")[1])};
+}
+
+
+void FirStage(){
+    std::string value = splice(command(("curl -H 'Authorization: Bearer " + key + "' " + CourseBase + "courses").c_str()), 2, 2);
+    std::vector<std::string> RawCourses;
+    std::string add;
+    int depth = 0;
+    for (int i = 0; i < value.length(); i++){
+        if (std::string("([{").contains(value[i])) depth++;
+        elif (std::string(")]}").contains(value[i])) depth--;
+        elif (depth == 0 && std::string(",").contains(value[i])){
+            RawCourses.push_back(add);
+            add = "";
+        }
+        else add += value[i];
+    }
+
+    for (int i = 0; i < RawCourses.size(); i++){
+        std::cout << RawCourses[i] << std::endl;
+        courses.push_back(ToCourse(RawCourses[i]));
+    }
+
+    stage.store(1);
+}
+
+
+void SecondStage(int i){
+    std::cout << command(("curl -H 'Authorization: Bearer " + key + "' " + CourseBase + "courses/" + std::to_string(i) + "/modules").c_str()) << std::endl;
+    stage.fetch_add(1);
 }
 
 
@@ -56,7 +161,6 @@ int main(int argc, char* argv[]) {
 
 
     /* Set os variable */
-
     #ifdef _WIN32
     os = 1;
     #elifdef __APPLE__
@@ -64,10 +168,6 @@ int main(int argc, char* argv[]) {
     #elifdef __linux__
     os = 3;
     #endif
-
-
-    std::string key = "4613~JW2h7AZXUK6LBkEnKxtPtmrJ22cUeUVGyfYCUhnuNNKat46nVAVN9ufUvaFUxvZA";
-    std::string CourseBase = "https://creanlutheran.instructure.com/api/v1/"; // conversations, courses
 
 
     if (os == 1) rpath = "./";
@@ -83,9 +183,9 @@ int main(int argc, char* argv[]) {
     SDL_SetRenderVSync(renderer, 1);
     SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
     SDL_StartTextInput(window);
-    std::cout << "Success! Initializing loop" << std::endl;
-    std::cout << "curl -H \\\"Authorization: Bearer 4613~JW2h7AZXUK6LBkEnKxtPtmrJ22cUeUVGyfYCUhnuNNKat46nVAVN9ufUvaFUxvZA\\\" \\\"https://creanlutheran.instructure.com/api/v1/conversations/94004\\\"" << std::endl;
-    std::cout << command("curl -H 'Authorization: Bearer 4613~JW2h7AZXUK6LBkEnKxtPtmrJ22cUeUVGyfYCUhnuNNKat46nVAVN9ufUvaFUxvZA' https://creanlutheran.instructure.com/api/v1/conversations/94004") << std::endl;
+    std::cout << "Successfully initialized SDL3" << std::endl;
+
+
     /* Initialize SDL_ttf, create font object */
     TTF_Init();
     TTF_Font * font = TTF_OpenFont((SDL_GetBasePath()+rpath+"Font.ttf").c_str(), 12);
@@ -93,16 +193,31 @@ int main(int argc, char* argv[]) {
 
     /* Init text assistant :) */
     TextCharacters Characters = {renderer, font, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890,.~!@#$%^&*()_+-=:;\"'? "};
-    TextObject Title = {"", Center, Center, Vector2(windowsize.x/2, windowsize.y/2), {255*darkmode, 255*darkmode, 255*darkmode, 255}, true};
+    TextObject Title = {"", Center, Center, Vector2(windowsize.x/2, windowsize.y/2), {(Uint8)(255*darkmode), (Uint8)(255*darkmode), (Uint8)(255*darkmode), 255}, true};
+
+
+    std::thread threads[tcount];
+    threads[0] = std::thread(FirStage);
 
 
     /* Main loop */
     while (loop){
-
-
         /* Get mouse pos and get FPS */
         deltime = (SDL_GetPerformanceCounter() - then) / (double)SDL_GetPerformanceFrequency();
         then = SDL_GetPerformanceCounter();
+
+
+        if (stage.load() == 1){
+            threads[0].join();
+            std::cout << "Got all courses" << std::endl;
+            stage.store(2);
+        }
+        elif (stage.load() == 2){
+            stage.fetch_add(1);
+            // for (int i = 0; i < tcount; i++){
+            //     threads[i] = std::thread(SecondStage, i);
+            // }
+        }
 
 
         /* Update mouse and input text */
